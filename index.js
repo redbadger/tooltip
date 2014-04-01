b5e8016f925eb92f1a16d80eedb10448f579b405
Alex Savin <karismafilms@gmail.com>
2014-04-01 11:54:30 +0100

    Tip position, fadeout animation, overlay tooltips for top and bottom

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var dom = require('dom');
 
 /*
-* Params list: target, content, type, position, overlay, timeout
+* Params list: target, content, type, position, overlay, timeout, tip, tipPosition
 */
+
 function Tooltip(params) {
   console.log(params.target);
   var el = dom(params.target)[0];
@@ -17,6 +18,22 @@ function Tooltip(params) {
   if(typeof params.tip !== 'undefined' && params.tip != false) {
     var tip = document.createElement('div');
     tip.className = 'tip';
+
+    switch(params.tipPosition) {
+      case 'left':
+        tip.style.left = '2%';
+        break;
+      case 'center':
+      case 'middle':
+        tip.style.left = '50%';
+        break;
+      case 'right':
+        tip.style.left = '97%';
+        break;
+      default:
+        tip.style.left = '50%';
+    }
+
     tooltip.appendChild(tip);
 
     var tipHeight = tooltip.offsetHeight;
@@ -25,21 +42,35 @@ function Tooltip(params) {
 
   var show = function() {
     if(this.position == 'top') {
-      dom(tip).addClass('top');
       var parentEl = this.el.parentNode;
       parentEl.insertBefore(tooltip, this.el);
       tooltip.style.top = '-5px';
-      tip.style.top = tooltip.offsetHeight;
+      if(typeof tip !== 'undefined') {
+        dom(tip).addClass('top');
+        tip.style.top = tooltip.offsetHeight;
+      }
+
+      if(this.overlay) {
+        // Calculating absolute position of the overlayed tooltip
+        var offsetTop = this.el.offsetTop - this.tooltip.offsetHeight - 5;
+        dom(this.tooltip).addClass('overlay');
+        dom(this.tooltip).css('top', offsetTop);
+      }
+
     } else {
-      dom(tip).addClass('bottom');
-      tip.style.top = '-5px';
       tooltip.style.top = '5px';
       dom(this.tooltip).insertAfter(this.el);
-    }
+      if(typeof tip !== 'undefined') {
+        dom(tip).addClass('bottom');
+        tip.style.top = '-5px';
+      }
 
-    if(this.overlay) {
-      console.log('Params overlay true');
-      dom(tooltip).addClass('overlay');
+      if(this.overlay) {
+        // Calculating absolute position of the overlayed tooltip
+        var offsetTop = this.el.offsetTop + this.el.offsetHeight + 5;
+        dom(this.tooltip).addClass('overlay');
+        dom(this.tooltip).css('top', offsetTop);
+      }
     }
 
     dom(this.tooltip).addClass('fadein');
@@ -51,6 +82,7 @@ function Tooltip(params) {
 
   var hide = function() {
     dom(tooltip).addClass('fadeout');
+    dom(tooltip).removeClass('fadein');
     window.setTimeout(function(){
       dom(tooltip).remove();
     }, 300);
@@ -63,6 +95,7 @@ function Tooltip(params) {
     el: el,
     overlay: params.overlay,
     position: params.position,
+    tip: tip,
     tipHeight: tipHeight
   };
 }
